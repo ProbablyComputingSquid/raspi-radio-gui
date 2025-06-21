f3baa8889485de8e22edf33a973ceb968f32a47d
Jay Bolling <jwbolling001@gmail.com>
2025-06-21 10:51:06 -0500

    .

---
+++ journal.md
@@ -0,0 +1,7 @@
+# 10:17 am 21/06/25
+
+trying to get things set up on the raspi
+
+10:25 am: imaging the usb drive lol, accidentally deleted it with arch :despair:
+
+10:43 am: setup pi, doing ssh now
---
+++ radiotest.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+def play_audio(file_path):
+    if not os.path.exists(file_path):
+        print(f"Error: The file {file_path} does not exist.")
+        sys.exit(1)
+
+    try:
+        if file_path.endswith('.wav'): # okay to pipe directly
+            os.execvp("sudo", ["sudo", "./pi_fm_rds", "-audio", file_path])
+        elif file_path.endswith('.mp3'): # need to decode first, use a sox pipe
+            os.execlp("sh", "sh", "-c", f"sox -t mp3 {file_path} -t wav - | sudo ./pi_fm_rds -audio -")
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+        sys.exit(1)
+def main():
+    play_audio("bad_apple_enhanced.mp3")
